02afc173464b287fb38542272ef0c31c006264c8
felipeFleite <felipefracalossileite@gmail.com>
2025-11-21 19:10:18 -0400

    z

--- js/main.js
+++ js/main.js
@@ -11,3 +11,42 @@ if ('serviceWorker' in navigator) {
     }
   });
 }
+
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+
+// capturando os elementos em tela
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraInvert = document.querySelector("#camera--invert");
+const cameraTrigger = document.querySelector("#camera--trigger");
+
+// Estabelecendo o acesso a câmera e inicializando a visualização
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            let track = stream.getTracks()[0];
+            cameraView.srcObject = stream
+        })
+        .catch(function (error) {
+            console.error("Ocorreu um Erro.", error);
+        });
+}
+
+// Função para tirar foto
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+cameraInvert.onclick = function () {
+constraints.video.facingMode = enviroment
+};
+
+// carrega imagem de câmera quando a janela carregar
+window.addEventListener("load", cameraStart, false);
